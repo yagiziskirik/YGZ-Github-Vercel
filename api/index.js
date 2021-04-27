062f2efafe76e10216353f28e531f286784402ab
yagiziskirik <y_iskirik_10@hotmail.com>
2021-04-27 10:13:35 +0300

    Test

--- api/index.js
+++ api/index.js
@@ -1,13 +1,9 @@
 const request = require('request');
 
-var data = null
-
-request('http://numbersapi.com/random/math', { json: false }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  data = body
-});
-
 module.exports = (req, res) => {
 	const name = req.query.name || 'stranger';
-	res.status(200).send(data)
+	request('http://numbersapi.com/random/math', { json: false }, (err, rest, body) => {
+		if (err) { return console.log(err); }
+		res.status(200).send(rest)
+	});
 }
