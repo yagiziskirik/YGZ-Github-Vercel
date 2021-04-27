9e26e9a48d28a0381dd4ee3aa4ffc7b8bcd9d88f
yagiziskirik <y_iskirik_10@hotmail.com>
2021-04-27 09:46:16 +0300

    Api call

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
+const https = require('https');
+
+https.get('http://numbersapi.com/random/math', (resp) => {
+  let data = '';
+
+  // A chunk of data has been received.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
 module.exports = (req, res) => {
 	const name = req.query.name || 'stranger';
 	res.status(200).send(`Hello, ${name}!`)
